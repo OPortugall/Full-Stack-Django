b915dbea18209093f7b831790d04346f6022a9b2
Oportugall <lemgruberportugalleonardo@gmail.com>
2026-07-13 23:21:18 -0300

    Forms com Django

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    subject = models.CharField(max_length=100)
+    message = models.CharField(max_length=250)
+    contact_email = models.EmailField()
+    urgent = models.BooleanField(null=True, blank=True)
